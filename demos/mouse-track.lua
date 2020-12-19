f9913bb5cb778bc4b77e6d6bb5f4e51a63a18afc
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-12-19 00:04:29 -0400

    show coords of cursor

--- demos/mouse-track.lua
+++ demos/mouse-track.lua
@@ -1,7 +1,10 @@
 events:On("mouseMove", function(x, y)
 	clear()
+
 	horizline(x / 4, 12) -- divided by window scale 
 	vertline(y / 4, 12)
+
+	print(string.format("(%03i, %03i)", x / 4, y / 4), 1, 1, 22)
 end)
 
 function Spin()end
