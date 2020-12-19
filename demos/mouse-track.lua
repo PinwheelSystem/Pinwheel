bb6263f2fb1e7539b78d26f3d4b8ee7b61ce0cf8
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-12-19 19:34:27 -0400

    fix line function args

--- demos/mouse-track.lua
+++ demos/mouse-track.lua
@@ -1,8 +1,8 @@
 events:On("mouseMove", function(x, y)
 	clear()
 
-	horizline(x / 4, 12) -- divided by window scale 
-	vertline(y / 4, 12)
+	horizline(y / 4, 12) -- divided by window scale 
+	vertline(x / 4, 12)
 
 	print(string.format("(%03i, %03i)", x / 4, y / 4), 1, 1, 22)
 end)
